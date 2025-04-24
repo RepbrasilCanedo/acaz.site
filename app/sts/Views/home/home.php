03bf610d7b37c4b315b11715062a4f3b57a0df1e
RepbrasilCanedo <administrativo@repbrasil.salvador.br>
2025-04-24 05:43:37 -0300

    Alinhamento das imagens na home

--- app/sts/Views/home/home.php
+++ app/sts/Views/home/home.php
@@ -6,20 +6,18 @@ if (!defined('R8P3B1R9S6L1')) {
 }
 ?>
 <body style="background: url(<?php echo URL; ?>app/sts/assets/images/Papel_Parede.jpg) left center;">
-  <!--  inicio da galeria de imagens-->
+  <!-- Início do Carousel superior-->
   <section class="top-carr p-0 m-0 w-100">
-   <div class="container-fluid p-0">
+   <div class="container-fluid p-0">      
       <div class="row no-gutters">
         <div class="col-md-12 p-0">         
-        <?php
-        // Acessa o IF quando encontrou algum registro no banco de dados
-        if (!empty($this->data['home']['carroussel'][0])) {
-            //foreach ($this->data['home']['carroussel'][0] as $home) {
-              extract($this->data['home']['carroussel'][0]);
-               //echo('<pre>');print_r($home); echo('</pre>');
-        ?>
-
-          <!-- Início do Carousel -->
+            <?php
+            // Acessa o IF quando encontrou algum registro no banco de dados
+            if (!empty($this->data['home']['carroussel'][0])) {
+                //foreach ($this->data['home']['carroussel'][0] as $home) {
+                  extract($this->data['home']['carroussel'][0]);
+                  //echo('<pre>');print_r($home); echo('</pre>');
+            ?>          
           <div id="carousel_imagens" class="carousel slide" data-ride="carousel" >
             <!--Início inner-->
             <div class="carousel-inner">
@@ -78,35 +76,31 @@ if (!defined('R8P3B1R9S6L1')) {
               </div>
             </div>
             <!--Final inner-->
-
             <!-- inicio controles -->
             <a href="#carousel_imagens" class="carousel-control-prev" data-slide="prev"><i class="fas fa-angle-left fa-3x"></i></a>
             <a href="#carousel_imagens" class="carousel-control-next" data-slide="next"><i class="fas fa-angle-right fa-3x"></i></a>
             <!-- Final dos controles -->
           </div>
-          <!-- Final do Carousel -->
-        <?php
-           //}
-        } else {
-            echo "<p style='color: #f00;'>Erro: Nenhum registro encontrado</p>";
-        }
-        ?>
+          <!-- Final do Carousel Superior-->
+          <?php
+            //}
+          } else {
+              echo "<p style='color: #f00;'>Erro: Nenhum registro encontrado</p>";
+          }
+          ?>
         </div>
       </div>
     </div>
   </section>
-    <!--  final da galeria de imgens-->
+  <!-- Final do Carousel superior-->
+
 <div class="container max-width">
-  <!--Inicio da segunda linha de divulgacao-->  
+  <!--Inicio da primwira linha de textos-->  
   <div class="row mt-5">
-    <!-- Linha da esquerda -->
     <div class="col md-8">
       <?php
-        // Acessa o IF quando encontrou algum registro no banco de dados
         if (!empty($this->data['home']['content'][0])) {
-            //foreach ($this->data['home']['carroussel'][0] as $home) {
               extract($this->data['home']['content'][0]);
-               //echo('<pre>');print_r($this->data['home']['content'][0]); echo('</pre>');
         ?>
       <section class="initial-article max-width centralizar mb-5">
        <article id="materia_inicial_col_esq">
@@ -124,16 +118,17 @@ if (!defined('R8P3B1R9S6L1')) {
             echo "<p style='color: #f00;'>Erro: Nenhum registro encontrado</p>";
         }
         ?>
-    </div>
+    </div>   
   </div>
-    <div class="row">
-    <!-- Linha da direita -->
+   <!--final da primwira linha de textos-->  
+
+  <!--Inicio da primwira linha de video e texto-->  
+  <div class="row">
     <div class="col centralizar md-4">
-      <?php
-        // Acessa o IF quando encontrou algum registro no banco de dados
-        if (!empty($this->data['home']['institucional'][0])) {
-              extract($this->data['home']['institucional'][0]);
-      ?>
+        <?php
+          if (!empty($this->data['home']['institucional'][0])) {
+                extract($this->data['home']['institucional'][0]);
+        ?>
        <section class="institucional max-width">        
         <h3 class="conteudo-produtos mb-3"><?php if (isset($title)) {echo $title;}?></h3>
         <article id="video">
@@ -155,25 +150,26 @@ if (!defined('R8P3B1R9S6L1')) {
         }
         ?>
     
+    </div>
   </div>
-  </div>
+  <!--final da primwira linha de video e texto--> 
 
+  <!--Inicio da primwira linha de divulgações-->
   <section class="divulgacao">
-
     <?php
-        // Acessa o IF quando encontrou algum registro no banco de dados
         if (!empty($this->data['home']['divulgacao'][0])) {
               extract($this->data['home']['divulgacao'][0]);
-    ?>
-    
-    <!-- Inicio linha de marketing-->
+    ?>    
+    <!--Inicio da primwira linha de Marketing-->
     <div class="row mt-5">
       <div class="col centralizar">
       <img src="<?php echo URLADM; ?>app/sts/assets/images/marketing/<?php if (isset($marketing_1)) {echo $marketing_1;}?>" alt="<?php if (isset($marketing_1_alt)) {echo $marketing_1_alt;}?>">
       </div>
     </div>
+    <!--Final da primwira linha de Marketing-->
+    
     <div class="row mt-3">
-      <div class="col md-12 conteudo-produtos" style="text-align:center;">
+      <div class="col-md-12 conteudo-produtos" style="text-align:center;">
         <h4 class="mt-2"><?php if (isset($title)) {echo $title;}?></h4>
       </div>
     </div>
@@ -206,7 +202,7 @@ if (!defined('R8P3B1R9S6L1')) {
 
     <div class="row mt-3">
       <!-- Divulgação segunda linha  coluna esquerda -->
-      <div class="col md-3  p-2" style="text-align:center;">
+      <div class="col-md-4  p-2" style="text-align:center;">
         <h5 class="conteudo-produtos"><?php if (isset($title_img_4)) {echo $title_img_4;}?></h5>
         <a href="<?php if (isset($url_image_4)) {echo $url_image_4;}?>" class="navbar-brand">
           <img src="<?php echo URLADM; ?>app/sts/assets/images/institucional/<?php if (isset($image_4)) {echo $image_4;}?>" alt="<?php if (isset($image_4_alt)) {echo $image_4_alt;}?>" title="<?php if (isset($image_3_title)) {echo $image_3_title;}?>">
@@ -214,7 +210,7 @@ if (!defined('R8P3B1R9S6L1')) {
       </div>
 
       <!-- Divulgação segunda linha  coluna centro -->
-      <div class="col md-3  p-2" style="text-align:center;">
+      <div class="col-md-4  p-2" style="text-align:center;">
         <h5 class="conteudo-produtos"><?php if (isset($title_img_5)) {echo $title_img_5;}?></h5>
         <a href="<?php if (isset($url_image_5)) {echo $url_image_5;}?>" class="navbar-brand">
           <img src="<?php echo URLADM; ?>app/sts/assets/images/institucional/<?php if (isset($image_5)) {echo $image_5;}?>" alt="<?php if (isset($image_5_alt)) {echo $image_5_alt;}?>" title="<?php if (isset($image_5_title)) {echo $image_5_title;}?>">
@@ -222,13 +218,12 @@ if (!defined('R8P3B1R9S6L1')) {
       </div>
 
       <!-- Divulgação segunda linha  coluna direita -->
-      <div class="col md-3  p-2" style="text-align:center;">
+      <div class="col-md-4  p-2" style="text-align:center;">
         <h5 class="conteudo-produtos"><?php if (isset($title_img_6)) {echo $title_img_6;}?></h5>
         <a href="<?php if (isset($url_image_6)) {echo $url_image_6;}?>" class="navbar-brand">
           <img src="<?php echo URLADM; ?>app/sts/assets/images/institucional/<?php if (isset($image_6)) {echo $image_6;}?>" alt="<?php if (isset($image_6_alt)) {echo $image_6_alt;}?>" title="<?php if (isset($image_6_title)) {echo $image_6_title;}?>">
         </a>
       </div>
-    </div>
     </div>
   </section>
 
